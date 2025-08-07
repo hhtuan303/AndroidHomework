daf1fe6e1f46a31d429b5e7e26e4d1297f53bd7d
hotuan30324 <hohoangtuan346@gmail.com>
2025-08-07 23:11:49 +0700

    update homework

---
+++ IntentFilter/app/src/main/java/com/example/intentfilter/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.intentfilter;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWeb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        myWeb = findViewById(R.id.my_web);
+        Intent myIntent = getIntent();
+        Uri myLink = myIntent.getData();
+        try {
+            myWeb.loadUrl(myLink.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
